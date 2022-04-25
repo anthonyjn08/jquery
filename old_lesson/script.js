74caa37fe4a9858eb15e70799c88f3db0fd5f1af
anthonyjn08 <anthonyjnelson@hotmail.com>
2022-04-25 19:30:58 +0000

    Lesson finished

--- old_lesson/script.js
+++ old_lesson/script.js
@@ -1,9 +1,10 @@
 $(document).ready(function () {
-    $("#stream1_btn").on("click", function () {
-        $(".stream1").removeClass('highlight_stream');
-        $(".stream2").removeClass('highlight_stream');
-        $(".stream3").removeClass('highlight_stream');
-        $(".stream1").addClass('highlight_stream');
+    $("#stream1_btn").on(function () {
+        $(".stream1").hide();
+        $(".stream1").hide('slow');
+        $(".stream1").hide('medium');
+        $(".stream1").hide('fast');
+        $(".stream1").hide(1000);
     });
     $("#stream2_btn").on("click", function () {
         $(".stream1").removeClass('highlight_stream');
@@ -16,5 +17,6 @@ $(document).ready(function () {
         $(".stream2").removeClass('highlight_stream');
         $(".stream3").removeClass('highlight_stream');
         $(".stream3").addClass('highlight_stream');
+        $("h2").on("hover")
     });
 });
